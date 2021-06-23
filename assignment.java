3d4d24f93ea4c075fdbad4b6e4b4cdc825e2a542
Rajan Chaurasiya <rajan2662000@gmail.com>
2021-06-23 21:49:54 +0530

    assignment operator added

---
+++ assignment.java
@@ -0,0 +1,8 @@
+public class assignment {
+    public static void main(String[] args) {
+        int x = 5;
+        // x *= 3;
+        // x += 4;
+        System.out.println(x < 5 && x < 3);
+    }
+}
