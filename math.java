7924467421aadfac0a82e0e09c0bfe555315c287
Rajan Chaurasiya <rajan2662000@gmail.com>
2021-06-24 11:25:58 +0530

    math method added

---
+++ math.java
@@ -0,0 +1,11 @@
+public class math{
+    public static void main(String[] args) {
+        System.out.println(Math.max(50, 20));
+        System.out.println(Math.min(50, 20));
+        System.out.println(Math.sqrt(50));
+        System.out.println(Math.abs(-50.87));
+        System.out.println(Math.random());
+    }
+
+}
+
