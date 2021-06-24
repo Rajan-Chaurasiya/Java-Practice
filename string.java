40a6c4da06cc3ea52548393890c3f0a73070477c
Rajan Chaurasiya <rajan2662000@gmail.com>
2021-06-24 11:25:39 +0530

    string method added

---
+++ string.java
@@ -0,0 +1,9 @@
+public class string {
+    public static void main(String[] args) {
+        String name = "abdhfgdgdfkgkh";
+        System.out.println("The length of string is: " + name.length());
+        System.out.println("The upper string is: " + name.toUpperCase());
+        System.out.println("The index of g is: " + name.indexOf("g"));
+    }
+    
+}
